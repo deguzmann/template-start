424e94d1a92a9ea099d47e407a841fba79e6b480
Evgeniy <samoshkinevgen@gmail.com>
2018-03-18 11:07:30 +0300

    Инициализация шаблона

---
+++ source/js/app.js
@@ -0,0 +1,19 @@
+var pageHeader = document.querySelector('.page-header');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+
+pageHeader.classList.remove('page-header--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.remove('page-header__toggle--closed');
+    navToggle.classList.add('page-header__toggle--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.remove('page-header__toggle--opened');
+    navToggle.classList.add('page-header__toggle--closed');
+  }
+});
